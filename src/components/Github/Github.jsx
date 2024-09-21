e19d0ec9bef4b744a9f96dedc2511ac88791f739
Shaan <shanali.ozhorik@Gmail.com>
2024-09-21 17:13:52 +0500

    added user card

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -13,12 +13,12 @@ const Github = () => {
   //   }, []);
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      <div className="w-auto h-72 flex flex-col gap-4 border">
-        Github Followers: {data.followers}
+      <div className="w-64 rounded-md h-72 flex flex-col gap-4 border p-4">
+        <h3 className="text-lg">Github Followers: {data.followers}</h3>
         <img
           src={data.avatar_url}
           alt="Git Picture"
-          className="w-28 h-28 rounded-full"
+          className="w-28 h-28 rounded-full  text-lg"
         />
       </div>
     </div>
