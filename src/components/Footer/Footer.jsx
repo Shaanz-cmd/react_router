83290576d538bf926e06851e8639918f5deae509
Shaan <shanali.ozhorik@Gmail.com>
2024-09-21 17:36:50 +0500

    github API added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/Shaanz-cmd"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -77,28 +77,12 @@ function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            <a href="https://github.com/Shaanz-cmd" className="hover:underline">
+              Shaanz-cmd
             </a>
             . All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <Link to="#" className="text-gray-500 hover:text-gray-900">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 8 19"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Facebook page</span>
-            </Link>
             <Link to="#" className="text-gray-500">
               <svg
                 className="w-4 h-4"
@@ -127,7 +111,7 @@ function Footer() {
               </svg>
               <span className="sr-only">Twitter page</span>
             </Link>
-            <Link to="#" className="text-gray-500">
+            <Link to="https://github.com/Shaanz-cmd" className="text-gray-500">
               <svg
                 className="w-4 h-4"
                 aria-hidden="true"
