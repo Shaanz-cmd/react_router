0eb4ec03b257ecc330c5262b4c6ec188cd47579a
Shaan <shanali.ozhorik@Gmail.com>
2024-09-21 17:30:16 +0500

    added user card

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -12,14 +12,15 @@ const Github = () => {
   //       });
   //   }, []);
   return (
-    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      <div className="w-64 rounded-md h-72 flex flex-col gap-4 border p-4">
-        <h3 className="text-lg">Github Followers: {data.followers}</h3>
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex justify-center">
+      <div className="w-64 rounded-md h-72 flex flex-col items-center gap-4 border p-4">
         <img
           src={data.avatar_url}
           alt="Git Picture"
           className="w-28 h-28 rounded-full  text-lg"
         />
+        <h3 className="text-lg">{data.name}</h3>
+        <h3 className="text-sm">Github Followers: {data.followers}</h3>
       </div>
     </div>
   );
@@ -28,6 +29,6 @@ const Github = () => {
 export default Github;
 
 export const githubLoader = async () => {
-  const response = await fetch("https://api.github.com/users/Shaanz-cmd");
+    const response = await fetch("https://api.github.com/users/Shaanz-cmd");
   return response.json();
 };
